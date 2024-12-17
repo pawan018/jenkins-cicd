960a55d106d023eba8e00c8d0648ceecaf153def
Pawan Chaurasia <pawan_chaurasia@epam.com>
2024-12-17 11:47:25 +0530

    first commit

---
+++ src/main/java/com/example/Service.java
@@ -0,0 +1,9 @@
+
+package com.example;
+
+public class Service {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Jenkins!");
+    }
+}
+        
---
+++ src/test/java/com/example/DummyAppTest.java
@@ -0,0 +1,13 @@
+
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DummyAppTest {
+    @Test
+    public void testMain() {
+        assertTrue(true, "Dummy test always passes!");
+    }
+}
+        
